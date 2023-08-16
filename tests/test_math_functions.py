9141519f136f7f6961408d04c9e9858328ad691e
Phuwasit Chokwiriyadit <inkkyhero@gmail.com>
2023-08-16 14:54:18 +0700

    testpass

--- tests/test_math_functions.py
+++ tests/test_math_functions.py
@@ -14,7 +14,7 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(10, 5), 5)
         self.assertEqual(subtract(0, 0), 0)
-        self.assertEqual(subtract(3, 5), -1)
+        self.assertEqual(subtract(3, 5), -2)
 
 if __name__ == '__main__':
     unittest.main()
