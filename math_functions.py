6db2eb1f28da16302dafc8e7a732da82dbacd380
Phuwasit Chokwiriyadit <inkkyhero@gmail.com>
2023-08-16 14:28:05 +0700

    First Commit

---
+++ math_functions.py
@@ -0,0 +1,7 @@
+# math_functions.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
---
+++ tests/test_calculator.py
@@ -0,0 +1,20 @@
+# tests/test_math_functions.py
+
+import unittest
+from math_functions import add, subtract
+
+class TestMathFunctions(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(0, 0), 0)
+        self.assertEqual(subtract(3, 5), -2)
+
+if __name__ == '__main__':
+    unittest.main()
